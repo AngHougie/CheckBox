47d9ea49a0213ad76053fdf857b807aaa57b0b39
AngHougie <angh-wm19@student.tarc.edu.my>
2020-07-11 15:18:41 +0800

    commit by anghougie

---
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.checkbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String tag = "Toppings: ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckboxClicked(View view) {
+
+        // is the view now checked.
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // check which checkbox is checked?
+        switch (view.getId()){
+            case R.id.checkBox_chocolate_syrup:
+                if (checked)
+                    tag = tag + "Chocolate syrup ";
+                break;
+            case R.id.checkBox_sprinkles:
+                if (checked)
+                    tag = tag + "Sprinkles ";
+                break;
+            case R.id.checkBox_crushed_nuts:
+                if (checked)
+                    tag = tag + "Crushed nuts ";
+                break;
+            case R.id.checkBox_cherries:
+                if (checked)
+                    tag = tag + "Cherries ";
+                break;
+            case R.id.checkBox_orio_cookie_crumbles:
+                if (checked)
+                    tag = tag + "Orio cookie crumbles ";
+                break;
+        }
+    }
+
+    public void tosMessage(String message){
+        Context context = getApplicationContext();
+        CharSequence msg = message;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, msg, duration);
+        toast.show();
+        tag = "Toppings: ";
+    }
+
+    public void showMsg(View view) {
+        tosMessage(tag);
+    }
+
+}
